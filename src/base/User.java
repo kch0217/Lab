db189088430d91c4d9494d526e99583006ebb232
Ken <kch0217@yahoo.com.hk>
2015-02-24 14:35:48 +0800

    1

--- src/base/User.java
+++ src/base/User.java
@@ -13,5 +13,35 @@ public class User {
 		this.email = email;
 		System.out.println("User [userId="+this.ID+", "+"userName="+this.name+", "+"userEmail="+this.email+"]");
 	}
+
+
+	public int getID() {
+		return ID;
+	}
+
+
+	public void setID(int iD) {
+		ID = iD;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 	
 }
